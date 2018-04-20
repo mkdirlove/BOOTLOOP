6f7a8cb4dbd8e64fa4c6e6ec7ec8a5f716c8d9b1
princeyasinzone <38537313+princeyasinzone@users.noreply.github.com>
2018-04-20 15:23:10 +0530

    Update cpscan.py

--- cpscan.py
+++ cpscan.py
@@ -31,7 +31,7 @@ def usage():
               _______  ___ _______ ____
              / __/ _ \(_-</ __/ _ `/ _ \.
              \__/ .__/___/\__/\_,_/_//_/
-               /_/ v 1.0 - susmithHCK
+               /_/ v 1.0 - Prince Yasin
     """
     print bcolors.ENDC
     print """
@@ -41,7 +41,7 @@ def usage():
          -h  --help     - show this menu
 
         EXAMPLE:
-          python cpscan.py -t targetsite.com -v
+          python2 cpscan.py -t targetsite.com -v
     """
     sys.exit()
 
