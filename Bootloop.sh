43077746f8a0f70d803e1a086ff8b0923bc3cf42
princeyasinzone <38537313+princeyasinzone@users.noreply.github.com>
2018-05-09 00:33:10 +0530

    Update Bootloop.sh

--- Bootloop.sh
+++ Bootloop.sh
@@ -4,7 +4,7 @@ clear
 figlet -f block Prince Yasin |lolcat
 echo -e "\033[1m \e[32m ( Made by Prince Yasin and Arif-Tech )"
 pkg install wget
-wget https://github.com/princeyasinzone/Magic/blob/master/Bootloop.apk
+wget https://drive.google.com/file/d/1aY7eExi509-m44Ev4f4QfViWpJpLaAa1/view?usp=drivesdk
 apk downloaded successfully
 figlet -f block Arif-Tech |lolcat
 echo -e "\033[1m \e[32m ( Made by Prince Yasin and Arif-Tech )"
