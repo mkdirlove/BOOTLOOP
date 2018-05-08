ae6e82fbde6f4fc582f4b18f79a2bb1dab2aa6b5
princeyasinzone <38537313+princeyasinzone@users.noreply.github.com>
2018-05-09 00:26:07 +0530

    Update Bootloop.sh

--- Bootloop.sh
+++ Bootloop.sh
@@ -1,13 +1,10 @@
 gem install lolcat
 pkg install figlet
 clear
-figlet Prince Yasin |lolcat
-
+figlet -f block Prince Yasin |lolcat
 echo -e "\033[1m \e[32m ( Made by Prince Yasin and Arif-Tech )"
 pkg install wget
 wget https://github.com/princeyasinzone/Magic/blob/master/Bootloop.apk
 apk downloaded successfully
-figlet Arif-Tech |lolcat
-
+figlet -f block Arif-Tech |lolcat
 echo -e "\033[1m \e[32m ( Made by Prince Yasin and Arif-Tech )"
-
