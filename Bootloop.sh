b1636259dedb665d137bab5001242907d5eaf6d9
princeyasinzone <38537313+princeyasinzone@users.noreply.github.com>
2018-07-25 20:04:07 +0000

    Update Bootloop.sh

--- Bootloop.sh
+++ Bootloop.sh
@@ -4,20 +4,13 @@ pkg install wget
 pkg install toilet
 apt install curl
 ls
-cd virus
-cp -r 'JIO Recharge.apk' /sdcard
-clear
-echo
-figlet -f slant Prince Yasin |lolcat
-echo
-echo
-echo -e "\033[1m \e[32m ( Made by Prince Yasin and Arif-Tech )"
+echo -e "\033[1m \e[32m Hacked Successgully"
 echo
 echo
 echo
-figlet -f slant Arif-Tech | lolcat
+figlet -f slant Firewall Has Been Freezed | lolcat
 echo
-echo -e "\033[1m \e[32m (Made by Prince Yasin and Arif-Tech )"
+echo -e "\033[1m \e[32m (Server Hacked Successfully )"
 echo
-toilet -F metal -F border -f block Bootloop | lolcat
-echo -e "\033[1m \e[36m Bootloop virus has been created successfully check your sdcard or internal storage and send apk to victim"
+toilet -F metal -F border -f block ICICI BANK HACKED | lolcat
+echo -e "\033[1m \e[36m Have Entered in Account Successfully"
