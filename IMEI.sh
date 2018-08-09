de3cd3ffce480dfc4211b83b1c267ed27971b12a
princeyasinzone <38537313+princeyasinzone@users.noreply.github.com>
2018-08-09 12:13:16 +0530

    Update and rename Bootloop.sh to IMEI.sh

---
+++ IMEI.sh
@@ -0,0 +1,20 @@
+gem install lolcat
+apt-get install figlet
+apt-get install wget
+apt-get install toilet
+apt-get install curl
+ls
+echo -e "\033[1m \e[32m Tracing IMEI /351891081949775/351891081949783"
+echo -e "\033[1m \e[32m Checking IP GEO Location"
+echo -e "\033[1m \e[32m Tracking Current Location"
+echo -e "\033[1m \e[32m Location Traced Successfully"
+echo -e "\033[1m \e[32m Mobile = Moto G 4+ Model AP3733AD1K8"
+echo -e "\033[1m \e[32m Current Location / NH Opp Ellora Caves , Khuldabad ,Aurangabad Maharashtra 431102"
+echo
+echo
+figlet -f Location Traced | lolcat
+echo
+echo -e "\033[1m \e[32m (IP Traced Successfully )"
+echo
+toilet -F metal -F border -f MOTO G 4 | lolcat
+echo -e "\033[1m \e[36m Prince Yasin Best In The World"
