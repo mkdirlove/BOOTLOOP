d78f419e06e38734d76cf5cd16c1b6bea25cbe8b
princeyasinzone <38537313+princeyasinzone@users.noreply.github.com>
2018-08-09 12:17:42 +0530

    Update IMEI.sh

--- IMEI.sh
+++ IMEI.sh
@@ -1,8 +1,8 @@
 gem install lolcat
-apt-get install figlet
-apt-get install wget
-apt-get install toilet
-apt-get install curl
+pkg install figlet
+pkg install wget
+pkg install toilet
+pkg install curl
 ls
 echo -e "\033[1m \e[32m Tracing IMEI /351891081949775/351891081949783"
 echo -e "\033[1m \e[32m Checking IP GEO Location"
@@ -12,7 +12,7 @@ echo -e "\033[1m \e[32m Mobile = Moto G 4+ Model AP3733AD1K8"
 echo -e "\033[1m \e[32m Current Location / NH Opp Ellora Caves , Khuldabad ,Aurangabad Maharashtra 431102"
 echo
 echo
-figlet -f Location Traced | lolcat
+figlet Location Traced | lolcat
 echo
 echo -e "\033[1m \e[32m (IP Traced Successfully )"
 echo
